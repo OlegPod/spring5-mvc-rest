4d689a57fec09664cc6d6e08042ec5fb38395f39
OlegPod <kepacuh@gmail.com>
2019-08-18 04:15:55 +0300

    Added Integration Test for PATCH Operation. Closes #7

---
+++ src/test/java/com/olehpodolin/spring5mvcrest/services/CustomerServiceImplIT.java
@@ -0,0 +1,111 @@
+package com.olehpodolin.spring5mvcrest.services;
+
+import com.olehpodolin.spring5mvcrest.api.v1.mapper.CustomerMapper;
+import com.olehpodolin.spring5mvcrest.api.v1.model.CustomerDTO;
+import com.olehpodolin.spring5mvcrest.bootstrap.Bootstrap;
+import com.olehpodolin.spring5mvcrest.domain.Customer;
+import com.olehpodolin.spring5mvcrest.repositories.CategoryRepository;
+import com.olehpodolin.spring5mvcrest.repositories.CustomerRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CustomerServiceImplIT {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    CustomerService customerService;
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("Loading Customer Data");
+        System.out.println(customerRepository.findAll().size());
+
+        //setup data for testing
+        Bootstrap bootstrap = new Bootstrap(categoryRepository, customerRepository);
+        bootstrap.run(); // loading data
+
+        customerService = new CustomerServiceImpl(CustomerMapper.INSTANCE, customerRepository);
+    }
+
+    @Test
+    public void patchCustomerUpdateFirstName() throws Exception {
+
+        //given
+        String updatedName = "Updated Name";
+        long id = getCustomerIdValue();
+
+        Customer originalCustomer = customerRepository.getOne(id);
+        assertNotNull(originalCustomer);
+        //save original first name
+        String originalFirstName = originalCustomer.getFirstname();
+        String originalLastName = originalCustomer.getLastname();
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setFirstname(updatedName);
+
+        customerService.patchCustomer(id, customerDTO);
+
+        //when
+        Customer updatedCustomer = customerRepository.findById(id).get();
+
+        //then
+        assertNotNull(updatedCustomer);
+        assertEquals(updatedName, updatedCustomer.getFirstname());
+        assertThat(originalFirstName, not(equalTo(updatedCustomer.getFirstname())));
+        assertThat(originalLastName, equalTo(updatedCustomer.getLastname()));
+    }
+
+    @Test
+    public void patchCustomerUpdateLastName() throws Exception {
+
+        //given
+        String updatedName = "Updated Name";
+        long id = getCustomerIdValue();
+
+        Customer originalCustomer = customerRepository.getOne(id);
+        assertNotNull(originalCustomer);
+
+        //save original first name
+        String originalFirstName = originalCustomer.getFirstname();
+        String originalLastName = originalCustomer.getLastname();
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setLastname(updatedName);
+
+        customerService.patchCustomer(id, customerDTO);
+
+        //when
+        Customer updatedCustomer = customerRepository.findById(id).get();
+
+        //then
+        assertNotNull(updatedCustomer);
+        assertEquals(updatedName, updatedCustomer.getLastname());
+        assertThat(originalFirstName, equalTo(updatedCustomer.getFirstname()));
+        assertThat(originalLastName, not(equalTo(updatedCustomer.getLastname())));
+    }
+
+    private Long getCustomerIdValue() {
+        List<Customer> customers = customerRepository.findAll();
+
+        System.out.println("Customers found: " + customers.size());
+
+        //return first id
+        return customers.get(0).getId();
+    }
+}
